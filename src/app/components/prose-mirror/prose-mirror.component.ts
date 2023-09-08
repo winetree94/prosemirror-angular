90b568398f03e011d41a6eb0e7e10750fd36a015
winetree94 <winetree94@gmail.com>
2023-09-08 18:00:59 +0900

    table

--- src/app/components/prose-mirror/prose-mirror.component.ts
+++ src/app/components/prose-mirror/prose-mirror.component.ts
@@ -25,6 +25,12 @@ import {
 import { buildBasicKeymap } from './plugins/keymaps/basic-keymaps';
 import { buildMenuItems } from './plugins/menu-bar/basic-menu-items';
 import { basicSchema } from './schema/basic';
+import {
+  columnResizing,
+  fixTables,
+  goToNextCell,
+  tableEditing,
+} from 'prosemirror-tables';
 
 @Component({
   selector: 'ng-prose-mirror',
@@ -52,40 +58,55 @@ export class ProseMirrorComponent implements OnInit {
   });
 
   public ngOnInit() {
-    this.view = new EditorView(this.editor.nativeElement, {
-      state: EditorState.create({
-        schema: this.mySchema,
-        plugins: [
-          // input 입력 시 동작할 것들
-          inputRules({
-            rules: [
-              ...smartQuotes,
-              ellipsis,
-              emDash,
-              blockQuoteRule(this.mySchema.nodes['blockquote']),
-              orderedListRule(this.mySchema.nodes['ordered_list']),
-              bulletListRule(this.mySchema.nodes['bullet_list']),
-              codeBlockRule(this.mySchema.nodes['code_block']),
-              headingRule(this.mySchema.nodes['heading'], 6),
-            ],
-          }),
-          // 키맵 설정
-          keymap(buildBasicKeymap(this.mySchema)),
-          keymap(baseKeymap),
-          dropCursor(),
-          gapCursor(),
-          menuBar({
-            floating: false,
-            content: buildMenuItems(this.mySchema).fullMenu,
-          }),
-          history(),
-          new Plugin({
-            props: {
-              attributes: { class: 'ProseMirror-Root-Class' },
-            },
-          }),
-        ],
-      }),
+    let state = EditorState.create({
+      schema: this.mySchema,
+      plugins: [
+        // table
+        columnResizing(),
+        tableEditing(),
+        keymap({
+          Tab: goToNextCell(1),
+          'Shift-Tab': goToNextCell(-1),
+        }),
+        // input 입력 시 동작할 것들
+        inputRules({
+          rules: [
+            ...smartQuotes,
+            ellipsis,
+            emDash,
+            blockQuoteRule(this.mySchema.nodes['blockquote']),
+            orderedListRule(this.mySchema.nodes['ordered_list']),
+            bulletListRule(this.mySchema.nodes['bullet_list']),
+            codeBlockRule(this.mySchema.nodes['code_block']),
+            headingRule(this.mySchema.nodes['heading'], 6),
+          ],
+        }),
+        // 키맵 설정
+        keymap(buildBasicKeymap(this.mySchema)),
+        keymap(baseKeymap),
+        dropCursor(),
+        gapCursor(),
+        menuBar({
+          floating: false,
+          content: buildMenuItems(this.mySchema).fullMenu,
+        }),
+        history(),
+        new Plugin({
+          props: {
+            attributes: { class: 'ProseMirror-Root-Class' },
+          },
+        }),
+      ],
     });
+
+    const fix = fixTables(state);
+    if (fix) state = state.apply(fix.setMeta('addToHistory', false));
+
+    this.view = new EditorView(this.editor.nativeElement, {
+      state: state,
+    });
+
+    document.execCommand('enableObjectResizing', false, 'false');
+    document.execCommand('enableInlineTableEditing', false, 'false');
   }
 }
