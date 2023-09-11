5e456a44054c9fcb047ba352d74d2b03d82dd1e6
winetree94 <winetree94@gmail.com>
2023-09-11 20:32:10 +0900

    d

--- src/app/components/prose-mirror/schema/marks/models.ts
+++
@@ -1,5 +0,0 @@
-import { MarkSpec } from 'prosemirror-model';
-
-export interface MarkSpecs {
-  [key: string]: MarkSpec;
-}
--- src/app/components/prose-mirror/schema/nodes/models.ts
+++
@@ -1,5 +0,0 @@
-import { NodeSpec } from 'prosemirror-model';
-
-export interface NodeSpecs {
-  [key: string]: NodeSpec;
-}
